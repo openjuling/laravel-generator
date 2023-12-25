ed326d19109ba5b6d1bdc4193e984a5b107202ea
wanganlin <inbox@wanganlin.com>
2023-12-25 09:29:59 +0800

    更新初始化脚本

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -14,22 +14,22 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'gen:install';
+    protected $signature = 'gen:init';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Install Code Ctl';
+    protected $description = 'Generator tool init';
 
     public function handle(): void
     {
         $fs = new Filesystem();
         $root = dirname(__DIR__, 3);
 
-        $fs->ensureDirectoryExists(app_path('Exceptions'));
-        $fs->copyDirectory($root.'/stubs/app/Exceptions', app_path('Exceptions'));
+        $fs->ensureDirectoryExists(app_path('Foundation/Exceptions'));
+        $fs->copyDirectory($root.'/stubs/app/Exceptions', app_path('Foundation/Exceptions'));
 
         $fs->ensureDirectoryExists(storage_path('app/ts/services'));
         $fs->ensureDirectoryExists(storage_path('app/ts/types'));
